f7ec6a594373921f1830a45c0fc2cb2c1e12eef8
Matias <hola@matiaspitters.com>
2019-11-01 11:49:54 +0100

    Create anime-test.js

---
+++ anime-test.js
@@ -0,0 +1,29 @@
+<script>
+
+$(document).ready(function() {
+  var animation = anime({
+  		targets: '.box',
+  		translateX: 250,
+      easing: 'linear',
+      autoplay: false
+		});
+  
+	$(window).scroll(function() {
+  	var windowBottom = $(this).scrollTop() + $(this).height();
+   	var elementTop = $('.site').offset().top;
+    var percentage = (windowBottom - elementTop) / $('.site').height() * 100;
+
+    if (percentage >= 100) {
+    	$('.percent').text('100%');
+    } else if (windowBottom >= elementTop) {
+    	$('.percent').text(`${Math.round(percentage)}%`);
+      animation.seek((percentage / 100) * animation.duration);
+    } else {
+      $('.percent').text('0%');
+    }
+  });
+	
+});
+
+
+</script>
